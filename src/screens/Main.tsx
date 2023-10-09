0fbb179e3867c0bd9654d85d4bd6ad3870e53b98
Park Minhyeong <105710796+duncan1409@users.noreply.github.com>
2023-10-09 23:37:20 +0900

    [Feat] add calendar screen
    use map and <FlatList/>

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
 import MyButton from '~/components/MyButton';
 
 export default function Main({navigation}: {navigation: any}) {
   return (
-    <View className="relative flex h-full items-center justify-center">
-      <Text className="text-2xl">This is the Main Screen</Text>
+    <View className="flex h-full items-center justify-center">
       <MyButton
         title="Go to Home"
         onPress={() => navigation.navigate('Home')}
       />
+      <View className=" h-64 ">
+        <FlatList
+          data={Array(28)
+            .fill(0)
+            .map((_, index) => ({key: index.toString()}))}
+          renderItem={({index}) => (
+            <View className="m-1">
+              <DayNote day={index + 1} />
+            </View>
+          )}
+          keyExtractor={item => item.key}
+          numColumns={7}
+        />
+      </View>
+    </View>
+  );
+}
+
+function DayNote({day}: {day: number}) {
+  return (
+    <View className="h-12 w-10 border-2 border-black">
+      <Text className="text-center">{day}</Text>
     </View>
   );
 }
